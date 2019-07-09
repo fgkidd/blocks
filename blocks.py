9d5780d5f5e7ee97addbe156d921d7b05445341a
fgkidd <fgkidd@ncsu.edu>
2019-07-09 16:25:11 -0400

    Update blocks.py

--- blocks.py
+++ blocks.py
@@ -2,5 +2,10 @@ import play
 
 
 class block():
-    def __init__(self,x,y,length,width):
-        self.object = play.new_box(color='red)
+    def __init__(self,x,y,width,height):
+        self.object = play.new_box(color='red',x=x,y=y,width=width,height=height)
+        self.object.start_physics(can_move=True,stable=False,x_speed=0,y_speed=0)
+        self.x=self.object.x
+        self.y=self.object.y
+        self.force=[0,0]
+
